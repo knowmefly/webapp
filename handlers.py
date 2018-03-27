7a68addaa88aa0e57cfc55d4e9f80802dc596bb6
knowmefly <30411613+knowmefly@users.noreply.github.com>
2018-03-27 18:04:22 +0800

    day7

--- handlers.py
+++ handlers.py
@@ -10,7 +10,15 @@ from coroweb import get,post
 from models import User,Comment,Blog,next_id
 @get('/')
 async def index(request):
-	users = await User.findAll()
+	# users = await User.findAll()
+	summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+	blogs = [
+		Blog(id='1', name='Test Blog', summary=summary,created_at=time.time()-120),
+		Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600 ),
+		Blog(id='3', name='There')
+	]
+	
+	
 	return{
 		'__template__':'test.html',
 		'users':users
